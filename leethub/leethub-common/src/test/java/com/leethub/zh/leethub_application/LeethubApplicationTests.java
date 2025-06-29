bdb0e3368f5c8ac59b78cb81dbfbd76eb200828e
jiaqi <879632300@qq.com>
2025-06-29 21:19:23 +0800

    项目搭建

---
+++ leethub/leethub-common/src/test/java/com/leethub/zh/leethub_application/LeethubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leethub.zh.leethub_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeethubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
