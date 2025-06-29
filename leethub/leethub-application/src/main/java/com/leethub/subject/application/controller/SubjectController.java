97e2ec1eb89f81361e22c6390076ed1eb38c6cdd
jiaqi <879632300@qq.com>
2025-06-29 22:53:21 +0800

    application

---
+++ leethub/leethub-application/src/main/java/com/leethub/subject/application/controller/SubjectController.java
@@ -0,0 +1,12 @@
+package com.leethub.subject.application.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SubjectController {
+  @GetMapping("/hello")
+  public String hello() {
+    return "Hello, World!";
+  }
+}
